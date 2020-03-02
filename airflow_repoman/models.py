fe1a12ac9ea66f8220669a94e7350f6a4dbf5f64
Gitea <gitea@fake.local>
2020-03-02 18:23:29 -0500

    TESTING

---
+++ airflow_repoman/models.py
@@ -0,0 +1,10 @@
+from airflow.models.base import Base
+from sqlalchemy import Column, Integer
+
+
+class Repos(Base):
+    __tablename__ = "repos"
+    test = Column(Integer, primary_key=True, autoincrement=True)
+
+
+Base.metadata.create_all()
