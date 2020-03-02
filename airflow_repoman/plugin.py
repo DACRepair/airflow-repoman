fe3e0df685a05dbab7d6b2197cb6b274908d3c71
Gitea <gitea@fake.local>
2020-03-02 16:49:47 -0500

    TESTING

--- airflow_repoman/plugin.py
+++ airflow_repoman/plugin.py
@@ -4,6 +4,6 @@ from .views import TestView
 
 class RepomanAirflowPlugin(AirflowPlugin):
     name = "airflow_repoman"
-    admin_views = [{"name": "Repos",
-                    "category": "Admin",
-                    "view": TestView()}]
+    appbuilder_views = [{"name": "Repos",
+                         "category": "Admin",
+                         "view": TestView()}]
