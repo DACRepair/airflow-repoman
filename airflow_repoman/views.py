198a6459ad1d749205dacff4b746c2231d029a3f
Gitea <gitea@fake.local>
2020-03-03 16:15:53 -0500

    TESTING

--- airflow_repoman/views.py
+++ airflow_repoman/views.py
@@ -3,23 +3,20 @@ from flask_appbuilder import ModelView
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from airflow_repoman.models import Repos
 
-RepomanBlueprint = Blueprint(
-    "airflow_repoman",
-    __name__,
-    template_folder="templates",
-    static_folder="static",
-    static_url_path="/static/airflow_repoman"
-)
+RepomanBlueprint = Blueprint("airflow_repoman",
+                             __name__,
+                             template_folder="templates",
+                             static_folder="static",
+                             static_url_path="/static/airflow_repoman")
 
 
 class RepomanView(ModelView):
     route_base = "/repos"
-
     datamodel = SQLAInterface(Repos)
 
     base_permissions = ['can_list']
+    search_columns = ['name']
 
     label_columns = {'name': 'Repo Name', 'enabled': 'Repo Enabled', 'remote_url': 'Remote URL',
                      'remote_branch': 'Remote Branch', 'refresh': 'Refresh (Seconds)', 'last_updated': 'Last Updated'}
-
     list_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'refresh', 'last_updated']
