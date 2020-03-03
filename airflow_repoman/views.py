8d2cc44a1e81f06992b5db38df2ea8908a2fea2c
Gitea <gitea@fake.local>
2020-03-03 18:13:59 -0500

    TESTING

--- airflow_repoman/views.py
+++ airflow_repoman/views.py
@@ -1,6 +1,11 @@
 from flask import Blueprint
 from flask_appbuilder import ModelView
+from flask_appbuilder.forms import DynamicForm
+from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
 from flask_appbuilder.models.sqla.interface import SQLAInterface
+from wtforms import Form, StringField
+from wtforms.validators import DataRequired
+
 from airflow_repoman.models import Repos
 
 RepomanBlueprint = Blueprint("airflow_repoman",
@@ -8,6 +13,10 @@ RepomanBlueprint = Blueprint("airflow_repoman",
                              template_folder="templates",
                              static_folder="static",
                              static_url_path="/static/airflow_repoman")
+
+
+class RepomanForm(DynamicForm):
+    remote_pass = StringField('Remote Password', description='Password', widget=BS3TextFieldWidget())
 
 
 class RepomanView(ModelView):
@@ -23,3 +32,5 @@ class RepomanView(ModelView):
     show_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'remote_user', 'remote_pass', 'refresh']
     add_columns = show_columns
     edit_columns = add_columns
+    add_form = RepomanForm
+    edit_form = RepomanForm
