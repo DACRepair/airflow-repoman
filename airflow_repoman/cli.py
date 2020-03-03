c86b958cbb02524ff2150f0fea660d203acf6e03
Gitea <gitea@fake.local>
2020-03-03 13:16:05 -0500

    TESTING

--- airflow_repoman/cli.py
+++ airflow_repoman/cli.py
@@ -1,4 +1,5 @@
 import click
+from airflow.logging_config import log
 
 
 @click.group()
@@ -6,9 +7,23 @@ def main():
     pass
 
 
-@main.command()
-def init():
+@main.command(help="Initializes the plugin db tables")
+def initdb():
     from airflow import settings
     from airflow_repoman.models import Base
 
-    Base.metadata.create_all(settings.engine)
+    log.info(Base.metadata.create_all(settings.engine))
+
+
+@main.command(help="")
+@click.option("")
+def reposync():
+    import os
+    from airflow import settings
+    from glob import glob
+
+    dag_path = settings.conf.get('core', 'dags_folder')
+
+
+if __name__ == "__main__":
+    main()
--- airflow_repoman/models.py
+++ airflow_repoman/models.py
@@ -1,7 +1,15 @@
 from airflow.models.base import Base
-from sqlalchemy import Column, Integer
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
 
 
 class Repos(Base):
     __tablename__ = "repos"
-    test = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255))
+    enabled = Column(Boolean, default=True)
+
+    remote_url = Column(String(2048))
+    remote_branch = Column(String(64), default='master')
+
+    refresh = Column(Integer, default=600)
+    last_updated = Column(DateTime)
