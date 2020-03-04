0b40ef516587341f25edcce2b3c7e1aab5eae497
Gitea <gitea@fake.local>
2020-03-03 21:48:30 -0500

    TESTING

--- airflow_repoman/views.py
+++ airflow_repoman/views.py
@@ -41,7 +41,8 @@ class RepomanView(ModelView):
 
     list_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'refresh', 'last_updated']
 
-    add_columns = edit_columns = show_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'remote_user',
-                                                 'remote_pass', 'refresh']
-    show_columns.append('last_updated')
-    add_form = edit_form = RepomanForm
+    # add_columns = edit_columns = show_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'remote_user',
+    #                                              'remote_pass', 'refresh']
+    add_columns = edit_columns = show_columns = ['name']
+    # show_columns.append('last_updated')
+    # add_form = edit_form = RepomanForm
