cdc0dcdcf824a5fc0a4cab97c9c2cd43ca47f164
Gitea <gitea@fake.local>
2020-03-03 21:43:43 -0500

    TESTING

--- airflow_repoman/views.py
+++ airflow_repoman/views.py
@@ -19,27 +19,29 @@ RepomanBlueprint = Blueprint("airflow_repoman",
 class RepomanForm(DynamicForm):
     name = StringField(lazy_gettext('Repo Name'), widget=BS3TextFieldWidget(), validators=[DataRequired()])
     enabled = BooleanField(lazy_gettext('Repo Enabled'))
-
     remote_url = StringField(lazy_gettext('Repo URL'), widget=BS3TextFieldWidget(), validators=[DataRequired()])
     remote_branch = StringField(lazy_gettext('Repo Branch'), widget=BS3TextFieldWidget(), validators=[DataRequired()])
     remote_user = StringField(lazy_gettext('Repo Username'), widget=BS3TextFieldWidget(), validators=[Optional()])
     remote_pass = PasswordField(lazy_gettext('Repo Password'), widget=BS3PasswordFieldWidget(), validators=[Optional()])
-
     refresh = IntegerField(lazy_gettext('Refresh Interval'), widget=BS3TextFieldWidget(),
                            validators=[NumberRange(min=0)])
 
 
 class RepomanView(ModelView):
-    route_base = "/repos"
+    route_base = "/repo"
     datamodel = SQLAInterface(Repos)
 
-    search_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'last_updated']
+    base_permissions = ['can_add', 'can_list', 'can_edit', 'can_delete']
+    base_order = ('name', 'asc')
+
     label_columns = {'name': 'Repo Name', 'enabled': 'Repo Enabled', 'remote_url': 'Remote URL',
                      'remote_branch': 'Remote Branch', 'refresh': 'Refresh (Seconds)', 'last_updated': 'Last Updated'}
-    list_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'refresh', 'last_updated']
-    show_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'remote_user', 'remote_pass', 'refresh']
-    add_columns = show_columns
-    edit_columns = add_columns
 
-    # add_form = RepomanForm
-    # edit_form = RepomanForm
+    search_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'last_updated']
+
+    list_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'refresh', 'last_updated']
+
+    add_columns = edit_columns = show_columns = ['name', 'enabled', 'remote_url', 'remote_branch', 'remote_user',
+                                                 'remote_pass', 'refresh']
+    show_columns.append('last_updated')
+    add_form = edit_form = RepomanForm
