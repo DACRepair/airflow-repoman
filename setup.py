494d9d016ca88e58979da476deb0e0c9b7b01836
Derek Vance <DACRepair@gmail.com>
2020-03-08 19:29:50 -0400

    Moved files around

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="airflow_repoman",
-    version="0.0.2",
+    version="0.0.3",
 
     author="Derek Vance",
     author_email="DACRepair@gmail.com",
