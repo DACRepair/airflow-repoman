366b225cc232ac091f1f4e90a3e57c178541531a
Derek Vance <DACRepair@gmail.com>
2020-03-09 21:42:31 -0400

    0.1.0 RC1 To Do: - Documentation - Some code cleanup

--- airflow_repoman/Airflow/admin.py
+++ airflow_repoman/Airflow/admin.py
@@ -1,10 +1,11 @@
 from flask_admin.contrib.sqla import ModelView
-from flask_admin.form import SecureForm
+# from flask_admin.form import SecureForm
 from wtforms.fields import PasswordField
 
 
 class DAGRepoAdminView(ModelView):
-    form_base_class = SecureForm
+    # form_base_class = SecureForm
+    can_view_details = True
 
     column_searchable_list = ['name', 'enabled', 'remote_url', 'remote_branch', 'last_updated']
     column_list = ['name', 'enabled', 'remote_url', 'remote_branch', 'interval', 'last_updated']
