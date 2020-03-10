4fac274544c21e292be79fae1a9fda0b01b3946b
Derek Vance <DACRepair@gmail.com>
2020-03-09 20:01:13 -0400

    Adding flask_admin support

--- airflow_repoman/Airflow/admin.py
+++ airflow_repoman/Airflow/admin.py
@@ -2,4 +2,5 @@ from flask_admin.contrib.sqla import ModelView
 
 
 class DAGRepoAdminView(ModelView):
-    column_list = ['name', 'enabled', 'remote_url', 'remote_branch', 'interval', 'last_updated']
+    column_searchable_list = ['name', 'enabled', 'remote_url', 'remote_branch', 'last_updated']
+    column_details_list = ['name', 'enabled', 'remote_url', 'remote_branch', 'interval', 'last_updated']
