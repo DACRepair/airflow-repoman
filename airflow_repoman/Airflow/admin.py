f6ce8827fae98017672446f62360e7e543d28590
Derek Vance <DACRepair@gmail.com>
2020-03-09 21:32:37 -0400

    0.1.0 RC1 To Do: - Documentation - Some code cleanup

--- airflow_repoman/Airflow/admin.py
+++ airflow_repoman/Airflow/admin.py
@@ -1,19 +1,13 @@
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.form import SecureForm
-from wtforms.fields import BooleanField, IntegerField, PasswordField, StringField
-
-
-class DAGRepoAdminForm(SecureForm):
-    name = StringField('Repo Name')
-    enabled = BooleanField('Repo Enabled')
-    remote_url = StringField('Repo URL')
-    remote_branch = StringField('Repo Branch')
-    remote_user = StringField('Repo Username')
-    remote_pass = PasswordField('Repo Password')
-    interval = IntegerField('Refresh Interval')
+from wtforms.fields import PasswordField
 
 
 class DAGRepoAdminView(ModelView):
+    form_base_class = SecureForm
+
     column_searchable_list = ['name', 'enabled', 'remote_url', 'remote_branch', 'last_updated']
     column_list = ['name', 'enabled', 'remote_url', 'remote_branch', 'interval', 'last_updated']
-    form = DAGRepoAdminForm
+
+    form_excluded_columns = ['id', 'last_checked', 'last_updated']
+    form_overrides = {'remote_pass': PasswordField}
